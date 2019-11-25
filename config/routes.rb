f135cbb33087a4d78a4c7c8940df6d4975899977
Terence30 <terence30360@gmail.com>
2019-11-25 15:40:06 +0100

    add controller Items + root + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	root :to =>  "items#index"
+	resources :items, only: [:index, :show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
