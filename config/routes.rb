394493897630a100dd63087cf67ba76b7ad17d4a
meladouz <marine.eladouzi@gmail.com>
2019-11-26 10:21:29 +0100

    controller cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	root :to =>  "items#index"
 	resources :items, only: [:index, :show]
   devise_for :users
+  resources :carts, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
