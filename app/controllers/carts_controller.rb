395edf28af510dbf804274192aa58913b2877598
Jordi-LG <jordy.leguet@gmail.com>
2019-11-26 17:14:08 +0100

    Total prices and delete item from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,12 +3,13 @@ class CartsController < ApplicationController
   def show
     @cart = Cart.find(params[:id])
     @items = all_items
+    @items_price = only_price_array
   end
 
   def update
     if current_user
       cart_exist?
-      @cart = current_user_card
+      @cart = current_user_cart
       @item = current_item
 
       if JoinCartItem.exists?(item_id: @item.id, cart_id: @cart.id) == false
@@ -27,14 +28,22 @@ class CartsController < ApplicationController
     end
   end
 
+  def destroy
+    JoinCartItem.delete(selected_item_in_cart)
+
+    redirect_to cart_path(Cart.find_by(user_id: current_user.id).id)
+  end
+
   private
+  ##########################
+  #METHODES POUR UPDATE
   def cart_exist?
     if Cart.exists?(user_id: current_user.id) == false
       Cart.create(user_id: current_user.id)
     end
   end
 
-  def current_user_card
+  def current_user_cart
     Cart.find_by(user_id: current_user.id)
   end
 
@@ -42,6 +51,8 @@ class CartsController < ApplicationController
     Item.find(params[:id])
   end
 
+  ##########################
+  #METHODES POUR SHOW
   def all_items
     u = current_user
     cu = Cart.find_by(user_id: u.id)
@@ -52,6 +63,24 @@ class CartsController < ApplicationController
     end
 
     return @items
+  end
+
+  def only_price_array
+    items = all_items
+
+    items_price = []
+    items.each do |item_price|
+      items_price << item_price.price
+    end
+
+    return items_price
+  end
+
+  #########################
+  #METHODE DELETE
+  def selected_item_in_cart
+    cu = Cart.find_by(user_id: current_user.id)
+    return JoinCartItem.find_by(cart_id: cu.id, item_id: params[:id])
   end
 
 end
