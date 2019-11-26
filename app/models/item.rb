03a6247f49483693b563dd8b324f22cf1e92e84f
Paulhennin <henninpaul@gmail.com>
2019-11-26 13:19:24 +0100

    validates for Item table

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,10 @@ class Item < ApplicationRecord
 
   has_many :join_order_items
   has_many :orders, through: :join_order_items
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true,
+                    format: { with: /\A\d+(\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+  validates :image_url, presence: true
 end
