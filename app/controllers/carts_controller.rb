b2f2774a297a33864a7a67ddca65ab041e895b29
meladouz <marine.eladouzi@gmail.com>
2019-11-26 13:59:38 +0100

    front show cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,22 @@
 class CartsController < ApplicationController
+
   def show
     @cart = Cart.find(params[:id])
+    @items = all_items
   end
+
+
+  private
+
+  def all_items
+    u = current_user
+    cu = Cart.find_by(user_id: u.id)
+    @items = []
+
+    JoinCartItem.where(cart_id: cu.id).each do |item|
+      @items << Item.find(item.item_id)
+    end
+    return @items
+  end
+
 end
